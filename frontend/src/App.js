8f4c944fd00d03f357eeda194fc107bb40788559
CaseyKohnen <Casey.Kohnen@gmail.com>
2023-08-27 18:24:21 -0500

    product screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
-import Header from "./componets/Header";
-import Footer from "./componets/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 const App = () => {
